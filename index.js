5a4bc6f604e7d6a7dd85cfe6736204c15d57d732
Ahmet Kök <kokahmet@pm.me>
2023-04-11 15:02:31 +0300

    tab index feature

--- index.js
+++ index.js
@@ -3,7 +3,14 @@ console.log("lets start");
 let fye = false;
 let btn = document.getElementById("fromyoureyesapp")
 let nodeList = document.querySelectorAll(".desc");
+let aproductList = document.querySelectorAll(".product-list-item");
+let aproducts = Array.from(aproductList);
 let elements = Array.from(nodeList);
+
+aproducts.forEach((aproduct) => {
+    console.log(aproduct);
+});
+
 elements.forEach((element) => {
     element.style.setProperty('--fye', 'none');
   });
